787942401df298a966c10759b284c1799adc1de9
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-11 19:19:08 +0200

    Create styled link component

---
+++ src/styled/Link.styled.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #ababab;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
