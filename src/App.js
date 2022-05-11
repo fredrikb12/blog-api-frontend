25d7f89cd4ee55a7b59712ee2a57e485acd3ff18
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-11 19:18:30 +0200

    Implement basic navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import { Outlet, Link } from "react-router-dom";
+import GlobalStyle from "./styled/GlobalStyle";
+import { StyledHomepage } from "./styled/Homepage.styled";
+import { StyledLink } from "./styled/Link.styled";
+import { StyledNav } from "./styled/Nav.styled";
+
 function App() {
-  return <div>Hello from app.</div>;
+  const [info, setInfo] = useState([]);
+  useEffect(() => {
+    async function fetchTest() {
+      fetch("https://guarded-mesa-79248.herokuapp.com/posts", {
+        credentials: "include",
+        method: "GET",
+        mode: "cors",
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+    fetchTest();
+  }, []);
+  return (
+    <>
+      <GlobalStyle />
+      <header>
+        <StyledNav>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/archive">Archive</StyledLink>
+          <StyledLink to="/about">About</StyledLink>
+        </StyledNav>
+      </header>
+      <StyledHomepage>
+        <Outlet />
+      </StyledHomepage>
+    </>
+  );
 }
 
 export default App;
