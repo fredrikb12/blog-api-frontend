93f86f547e0d6ccc1b51ed93f6ed78277eea7e2e
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-11 21:04:11 +0200

    Style articles, global, homepage

--- src/styled/Article.styled.js
+++ src/styled/Article.styled.js
@@ -4,4 +4,15 @@ export const StyledArticle = styled.article`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
+  border: 4px solid;
+  border-radius: 12px;
+  padding: 10px 20px;
+  text-align: center;
+  max-width: clamp(250px, 80vw, 500px);
+  &:nth-child(odd) {
+    border-color: #76a297;
+  }
+  &:nth-child(even) {
+    border-color: #2b1820;
+  }
+`;
--- src/styled/Homepage.styled.js
+++ src/styled/Homepage.styled.js
@@ -3,5 +3,7 @@ import styled from "styled-components";
 export const StyledHomepage = styled.main`
   display: flex;
   flex-direction: column;
-  
+  align-items: center;
+  gap: 15px;
+  padding: 10px;
 `
