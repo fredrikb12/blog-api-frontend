ededb00f171b2fb39a30498a5a9462d7c461ac5d
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-11 12:26:13 +0200

    Add routing and fix file structure

---
+++ src/components/RouteSwitch.js
@@ -0,0 +1,14 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+
+function RouteSwitch() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default RouteSwitch;
