bdef93e55c45e9e0060516f244beecfc7116a94e
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-12 19:10:42 +0200

    Add about route/component

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
+import About from "./About";
 import Archive from "./Archive";
 import Homepage from "./Homepage";
 
@@ -10,6 +11,7 @@ function RouteSwitch() {
         <Route path="/" element={<App />}>
           <Route index element={<Homepage />} />
           <Route path="/archive" element={<Archive />} />
+          <Route path="/about" element={<About/>} />
         </Route>
       </Routes>
     </HashRouter>
