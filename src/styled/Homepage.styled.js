7b5c6646f38d466873b4c3436c47b889ac8e9897
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-13 13:55:15 +0200

    Style homepage depending on screen size

--- src/styled/Homepage.styled.js
+++ src/styled/Homepage.styled.js
@@ -6,4 +6,20 @@ export const StyledHomepage = styled.main`
   align-items: center;
   gap: 25px;
   padding: 10px;
-`
+
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+
+    & article {
+      width: clamp(300px, 30vw, 450px);
+    }
+  }
+
+  @media(min-width: 1024px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-items: center;
+  }
+`;
