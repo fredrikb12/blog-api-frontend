b877c808c6a4e51b1af9dae5450d8b0e61a14b34
fredrikb12 <fredrik.backman12@gmail.com>
2022-05-23 19:58:07 +0200

    Add ability to write new comments and display them

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,6 +4,63 @@ import { useParams } from "react-router-dom";
 function Post() {
   const { postId } = useParams();
   const [post, setPost] = useState(null);
+  const [commentAuthor, setCommentAuthor] = useState("");
+  const [commentText, setCommentText] = useState("");
+  const [updateMessage, setUpdateMessage] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(JSON.stringify({ text: commentText, author: commentAuthor }));
+    try {
+      const res = await fetch(
+        `http://localhost:3000/posts/${postId}/comments`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          method: "POST",
+          mode: "cors",
+          body: JSON.stringify({ text: commentText, author: commentAuthor }),
+        }
+      );
+      if (res.status === 200) {
+        const data = await res.json();
+        setUpdateMessage(() => data.message);
+        setPost((prevPost) => {
+          console.log(data);
+          return {
+            ...prevPost,
+            comments: [
+              ...prevPost.comments,
+              { text: commentText, author: commentAuthor, id: data.id },
+            ],
+          };
+        });
+        setCommentAuthor(() => "");
+        setCommentText(() => "");
+      } else {
+        throw new Error(res.status);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function handleChange(e) {
+    switch (e.target.name) {
+      case "author": {
+        setCommentAuthor(() => e.target.value);
+        break;
+      }
+      case "text": {
+        setCommentText(() => e.target.value);
+        break;
+      }
+      default:
+        break;
+    }
+  }
 
   useEffect(() => {
     async function fetchPost() {
@@ -14,6 +71,7 @@ function Post() {
           mode: "cors",
         });
         if (res.status === 200) {
+          console.log(res);
           const post = await res.json();
           console.log(post);
           setPost(() => post);
@@ -21,7 +79,7 @@ function Post() {
           throw new Error(res.status + ": Something went wrong ");
         }
       } catch (e) {
-        console.log(e);
+        //console.log(e);
       }
     }
 
@@ -42,6 +100,54 @@ function Post() {
           </p>
         </div>
         <p>{post.text}</p>
+        {post.comments.length > 0 && <hr style={{ margin: "25px 0" }}></hr>}
+        {post.comments.map((comment, index) => {
+          return (
+            <article key={comment._id || index}>
+              <p>{comment.author}</p>
+              <p>{comment.text}</p>
+            </article>
+          );
+        })}
+        <hr style={{ margin: "25px 0" }}></hr>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: "grid",
+            gridTemplateColumns: "150px 1fr",
+            maxWidth: "800px",
+            rowGap: "20px",
+          }}
+        >
+          <label htmlFor="author">Name:</label>
+          <input
+            type="text"
+            name="author"
+            value={commentAuthor}
+            onChange={handleChange}
+            required
+            style={{
+              backgroundColor: "darkslategrey",
+              border: "none",
+              padding: "10px",
+            }}
+          />
+          <label htmlFor="text">Text:</label>
+          <textarea
+            name="text"
+            value={commentText}
+            onChange={handleChange}
+            required
+            style={{
+              backgroundColor: "darkslategrey",
+              border: "none",
+              padding: "10px",
+            }}
+          />
+          <button type="submit" style={{ color: "black" }}>
+            Comment
+          </button>
+        </form>
       </div>
     );
   }
